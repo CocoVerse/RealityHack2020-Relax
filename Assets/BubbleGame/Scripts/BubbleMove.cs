a7e7658fb29d186de3433b21fbe2c70fbc492f64
Wiley <wileycorning@gmail.com>
2020-01-10 17:20:05 -0500

    Bubble pop effect

--- Assets/BubbleGame/Scripts/BubbleMove.cs
+++ Assets/BubbleGame/Scripts/BubbleMove.cs
@@ -2,21 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class BubbleMove : MonoBehaviour
 {
     [SerializeField] BubbleColorCode colorCode;
+    [SerializeField] GameObject spawnOnPop;
+
     public BubbleColorCode ColorCode => colorCode;
 
     [SerializeField] float lifespan = 2f;
     [SerializeField] float speed = 0.5f;
+    private float startTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, lifespan);
+        startTime = Time.time;
     }
 
     internal void Pop(bool good) {
+        if (spawnOnPop != null) {
+            var obj = Instantiate(spawnOnPop, transform.position, transform.rotation);
+            obj.transform.localScale = transform.localScale;
+        }
         Destroy(gameObject);
     }
 
@@ -24,5 +31,6 @@ public class BubbleMove : MonoBehaviour
     void Update()
     {
         transform.position += transform.forward * Time.deltaTime * speed;
+        if (Time.time > startTime + lifespan) Pop(false);
     }
 }
---
+++ Assets/BubbleGame/Scripts/OneOffVisualEffect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+public class OneOffVisualEffect : MonoBehaviour {
+    [SerializeField] float lifespan;
+
+    [SerializeField] VisualEffect vfx;
+
+    private void Start() {
+        Destroy(gameObject, lifespan);
+        vfx.Play();
+    }
+
+}
