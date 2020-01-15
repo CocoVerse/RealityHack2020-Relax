b2e79af95b6ff88f160161b8f40d4d3667b5d124
Wiley <wileycorning@gmail.com>
2020-01-15 16:47:59 -0500

    Simple animation loop for all characters

---
+++ Assets/RandomAnimationSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimationSelector : StateMachineBehaviour {
+    [SerializeField] string parameterName = "RandomIndex";
+    [SerializeField] int min_inclusive = 0;
+    [SerializeField] int max_exclusive = 1;
+
+    public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash) {
+        animator.SetInteger(parameterName, UnityEngine.Random.Range(min_inclusive, max_exclusive));
+    }
+}
